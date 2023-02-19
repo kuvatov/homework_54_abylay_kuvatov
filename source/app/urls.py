52d5c9f818d116d74ac3c5992d20c692855d159d
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-19 10:25:04 +0600

    Добавил возможность редактирования и удаления категорий

--- source/app/urls.py
+++ source/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from webapp.views.base import home_view
-from webapp.views.categories import category_add_view
+from webapp.views.categories import category_add_view, categories_view, category_edit, category_delete
 from webapp.views.products import products_view, product_view, product_add_view
 
 urlpatterns = [
@@ -25,5 +25,8 @@ urlpatterns = [
     path("products", products_view, name='products_view'),
     path("products/<int:pk>", product_view, name='product_view'),
     path("categories/add", category_add_view, name='category_add_view'),
-    path("products/add", product_add_view, name='product_add_view')
+    path("products/add", product_add_view, name='product_add_view'),
+    path("categories", categories_view, name='categories_view'),
+    path("categories/<int:pk>/edit", category_edit, name='category_edit_view'),
+    path("categories/<int:pk>", category_delete, name='category_delete')
 ]
--- source/webapp/views/categories.py
+++ source/webapp/views/categories.py
@@ -1,5 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Category
 
@@ -16,3 +16,30 @@ def category_add_view(request: WSGIRequest):
     }
     Category.objects.create(**context)
     return redirect('products_view')
+
+
+def categories_view(request: WSGIRequest):
+    categories = Category.objects.all()
+    return render(request, 'categories_view.html', context={
+        'categories': categories
+    })
+
+
+def category_edit(request: WSGIRequest, pk: int):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'category_edit_view.html', context={
+            'category': category
+        })
+    context = {
+        'name': request.POST.get('name'),
+        'description': request.POST.get('description')
+    }
+    Category.objects.filter(pk=pk).update(**context)
+    return redirect('categories_view')
+
+
+def category_delete(request: WSGIRequest, pk: int):
+    category = Category.objects.get(pk=pk)
+    category.delete()
+    return redirect('categories_view')
